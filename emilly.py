dfa277bf30f931eca1ca6467156b5fb250f917f0
Emilly <emillylais1234@gmail.com>
2025-03-18 09:05:58 -0300

    Create emilly.py

---
+++ emilly.py
@@ -0,0 +1,15 @@
+nome = input ("Digite seu nome:")
+print ("Olá,", nome)
+idade = int(input ("Digite sua idade:"))
+print (idade)
+cidade = input ("Digite sua cidade:")
+print (cidade)
+comida = input ("Digite sua comida preferida:")
+print (comida)
+hobbie = input ("Digite seu hobbie favorito:")
+print (hobbie)
+
+print(f"{nome}, era uma pessoa muito legal, que tem {idade} anos e ele(a) mora em {cidade}. Um dia sua mãe fez sua comida preferida, que era {comida}, mas ele tinha um compromisso muito importante. No caminho, ele fez o que ele mais gosta, que é {hobbie}, cumpriu seu compromisso e voltou para casa e comeu {comida}.")
+
+
+
